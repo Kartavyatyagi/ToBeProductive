1df909799e7989fd1f7ae40caeb6e6a4adbbc8d7
kartavyatyagi <kartavyatyagi453@gmail.com>
2024-11-12 22:18:42 +0530

    android project 1

---
+++ app/src/main/java/com/example/tobeproductive/ModifyData.java
@@ -0,0 +1,18 @@
+package com.example.tobeproductive;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ModifyData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_data);
+    }
+}
